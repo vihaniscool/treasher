7123f3834e53ed0e30bbfceb843ffbf037da9ae3
vihaniscool <75142511+vihaniscool@users.noreply.github.com>
2022-03-17 16:56:42 -0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,31 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "SKETCH.JS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("E H C S J T K S . ", 700,330)
+    fill("lightblue")
+    text("Hint: Main Area of Coding", 700,350)
+}
